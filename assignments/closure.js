975a39d46f881360e1f9adba7a1e0ecb3d833a0c
Zach Taylor <zbtaylor1@gmail.com>
2019-05-26 19:56:16 -0400

    complete the basic closure challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const myHobby = "Cycling";
+
+const yell = () => {
+  console.log(`${myHobby} is a blast! But also a money pit...`);
+}
+
+yell();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
